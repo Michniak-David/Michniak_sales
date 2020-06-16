4064229051805ebf260ddf9b6d42f69552231939
David <David@David-PC>
2020-06-16 15:19:21 +0200

    Customer.java added

---
+++ src/Customer.java
@@ -0,0 +1,24 @@
+
+public class Customer {
+	
+	private int customerID;
+	private String firstName;
+	
+	
+	public int getCustomerID() {
+		return customerID;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+}
